a508dd8506a5d0866c1a473cdb52a837ceaadfe4
Otto Lote <otto.lote@innofactor.com>
2024-04-22 11:38:03 +0200

    Run formatter

--- publisher/main.py
+++ publisher/main.py
@@ -10,8 +10,10 @@ logging.basicConfig(level=logging.INFO)
 
 # Parse arguments with LOGIN and PASSWORD for Confluence
 parser = argparse.ArgumentParser()
-parser.add_argument('--login', help='Login with "" is mandatory', required=True)
-parser.add_argument('--password', help='Password with "" is mandatory',  required=True)
+parser.add_argument(
+    '--login', help='Login with "" is mandatory', required=True)
+parser.add_argument(
+    '--password', help='Password with "" is mandatory',  required=True)
 args = parser.parse_args()
 inputArguments = vars(args)
 
@@ -20,9 +22,11 @@ CONFIG = getConfig()
 
 logging.debug(CONFIG)
 
-pages = searchPages(login=inputArguments['login'], password=inputArguments['password'])
-deletePages(pagesIDList=pages, login=inputArguments['login'], password=inputArguments['password'])
+pages = searchPages(
+    login=inputArguments['login'], password=inputArguments['password'])
+deletePages(pagesIDList=pages,
+            login=inputArguments['login'], password=inputArguments['password'])
 
-publishFolder(folder = str(CONFIG["github_folder_with_md_files"]), 
-  login=inputArguments['login'], 
-  password=inputArguments['password'])
+publishFolder(folder=str(CONFIG["github_folder_with_md_files"]),
+              login=inputArguments['login'],
+              password=inputArguments['password'])
