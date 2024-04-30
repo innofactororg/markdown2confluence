ec84940de18ecb073d3d234076b704a02031fde3
Otto Lote <otto.lote@innofactor.com>
2024-04-30 12:04:14 +0200

    Add page title suffix and page label to config

---
+++ markdown2confluence/__init_.py
@@ -0,0 +1,10 @@
+"""
+This is the __init__.py file for the markdown2confluence package.
+It exposes the relevant classes and functions to be used by external modules.
+"""
+
+from .main import main
+from .converter import Converter
+from .publisher import Publisher
+from .config import Config
+
--- markdown2confluence/publisher.py
+++ markdown2confluence/publisher.py
@@ -2,7 +2,8 @@ from atlassian import Confluence
 
 
 class Publisher:
-    def __init__(self, url, username, password, space_id, parent_page_id):
+    def __init__(self, url, username, password, space_id,
+                 parent_page_id, page_title_suffix, page_label):
         self.confluence = Confluence(
             url=url,
             username=username,
@@ -10,22 +11,39 @@ class Publisher:
         )
         self.space_id = space_id
         self.parent_page_id = parent_page_id
+        self.page_title_suffix = page_title_suffix
+        self.page_label = page_label
 
     def publish_page(self, title, content):
-        return self.confluence.create_page(
+        title_with_suffix = f"{title}{self.page_title_suffix}"
+        existing_page = self.confluence.get_page_by_title(
             space=self.space_id,
-            title=title,
-            body=content,
-            parent_id=self.parent_page_id,
-            type='page'
+            title=title_with_suffix,
+            expand='version'
         )
+        if existing_page:
+            return
+            # return self.update_page(
+            #     page_id=existing_page['id'],
+            #     title=title_with_suffix,
+            #     content=content
+            # )
+        else:
+            return self.confluence.create_page(
+                space=self.space_id,
+                title=title_with_suffix,
+                body=content,
+                parent_id=self.parent_page_id,
+                type='page'
+            )
 
     def update_page(self, page_id, title, content):
         return self.confluence.update_page(
             page_id=page_id,
             title=title,
             body=content,
-            type='page'
+            type='page',
         )
 
-    # Additional methods for Publisher functionality could be added here
+    def delete_page(self, page_id):
+        return self.confluence.remove_page(page_id)
