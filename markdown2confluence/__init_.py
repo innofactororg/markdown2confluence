8d766d506c59c2c1388236c499d96f924f45c4c8
Otto Lote <otto.lote@innofactor.com>
2024-04-30 12:30:01 +0200

    sq

--- markdown2confluence/__init_.py
+++ markdown2confluence/__init_.py
@@ -5,5 +5,6 @@ It exposes the relevant classes and functions to be used by external modules.
 
 from .main import main
 from .converter import Converter
-from .publisher import Publisher, Config
+from .publisher import Publisher
+from .config import Config
 
