49670829b55ba42af121220fe87f70af01553e51
Otto Lote <otto.lote@innofactor.com>
2024-05-23 16:06:20 +0200

    Update __init__.py

--- markdown2confluence/__init_.py
+++ markdown2confluence/__init_.py
@@ -6,5 +6,8 @@ It exposes the relevant classes and functions to be used by external modules.
 from .main import main
 from .converter import Converter
 from .publisher import Publisher
+from .confluence import ConfluencePublisher
+from .parser import Parser, MarkdownParser
 from .config import Config
 from .util import Logger
+from .content_tree import ContentTree, ContentNode
